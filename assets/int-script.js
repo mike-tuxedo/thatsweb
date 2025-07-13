0738631b20acfcd9614fd4b3c1272ff619803456
mike-tuxedo <michael.tiefenthaler@tutanota.com>
2025-07-13 09:06:05 +0200

    Added styling

--- assets/int-script.js
+++ assets/int-script.js
@@ -1,1 +1,14 @@
-console.log('init skript loaded')
+console.log('init skript loaded');
+
+const $ = (query) => document.querySelector(query);
+const $$ = (query) => document.querySelectorAll(query);
+
+function toggleMobileMenu() {
+    const menuLevel1 = $('header nav > ul');
+
+    menuLevel1.classList.toggle('show');
+}
+
+window.addEventListener('load', () => {
+
+})
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
     <link rel="stylesheet" href="/assets/init-styles.css">
-    <link rel='stylesheet' href='/assets/pico.lime.min.css'>
+    <link rel='stylesheet' href='/assets/pico.cyan.min.css'>
     <script src="/assets/int-script.js"></script>
 
     <script>
@@ -29,7 +29,7 @@
 
         const contentLoaded = new Event('contentLoaded');
         // Alle Komponenten laden, wenn die Seite geladen ist
-        document.addEventListener('DOMContentLoaded', async function () {
+        document.addEventListener('DOMContentLoaded', async function() {
             const path = location.pathname !== '/' ? location.pathname : '/index';
             await loadComponent(`${path}/main-content.html`, 'main-container');
             await loadComponent('/footer-content.html', 'footer-container');
@@ -42,25 +42,41 @@
 </head>
 
 <body>
+<div class="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
     <header id="header-container">
-		<?php include('header-content.html'); ?>
+        <?php include('header-content.html'); ?>
     </header>
 
     <main id="content-container">
         <div id="top-container" class="content-full">
-			<?php
-			$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-			if ($path === '/') {
-				$fullPath = $_SERVER['DOCUMENT_ROOT'] . '/index/top-content.html';
-			} else {
-				$topContentPath = $path . '/top-content.html';
-				$fullPath = $_SERVER['DOCUMENT_ROOT'] . $topContentPath;
-			}
-			if (file_exists($fullPath)) {
-				include $fullPath;
-			}
-			?>
-		</div>
+            <?php
+            $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+            if ($path === '/') {
+                $fullPath = $_SERVER['DOCUMENT_ROOT'] . '/index/top-content.html';
+            } else {
+                $topContentPath = $path . '/top-content.html';
+                $fullPath = $_SERVER['DOCUMENT_ROOT'] . $topContentPath;
+            }
+            if (file_exists($fullPath)) {
+                include $fullPath;
+            }
+            ?>
+        </div>
         <div id="main-container" class="content-md"></div>
     </main>
 
